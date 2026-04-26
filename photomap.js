5ba72ca8ce8756630ed308186f5e22f9fa301962
aelami1 <aelami1@lsu.edu>
2026-04-26 17:44:12 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -21,8 +21,8 @@ function createPhotoMap () {
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
   var basemap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-  	minZoom: 0
-    maxZoom: 20,
+  	minZoom: 0,
+	maxZoom: 20,
   	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
  ext: 'jpg'
 }).addTo(map);
